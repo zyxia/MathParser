a07b8fa2446a92f5ab8c1b3cac73e2f8eef6ed14
xiazeye <969919860@qq.com>
2022-06-25 19:53:45 +0800

    use StaticTool to transfer string

--- TokenParser/Test.TokenParser/TreeBuild.cs
+++ TokenParser/Test.TokenParser/TreeBuild.cs
@@ -4,6 +4,7 @@ using System.IO;
 using MathParser;
 using Newtonsoft.Json;
 using NUnit.Framework;
+using TokenParser;
 
 namespace Test.TokenParser
 {
@@ -12,7 +13,7 @@ namespace Test.TokenParser
         [Test]
         public void BuildTest()
         {
-            string value = "(sin(x)-cos(y))";
+            var value = "(sin(x)-cos(y))";
             TestMathFormat(value);
             value = "(sin(x)-cos(y-x))";
             TestMathFormat(value);
@@ -25,22 +26,13 @@ namespace Test.TokenParser
         [Test]
         public void BuildTest2()
         {
-            string value = "2*sin(t)*cos(1.570796)";
+            const string value = "2*sin(t)*cos(1.570796)";
             TestMathFormat(value);
         }
 
         private static void TestMathFormat(string value)
         {
-            var stream = new MemoryStream();
-            var writer = new StreamWriter(stream);
-            writer.WriteLine(value);
-            writer.Flush();
-            stream.Seek(0, SeekOrigin.Begin);
-            var scanner = new Scanner(stream);
-            var parser = new Parser(scanner);
-            Assert.IsTrue(parser.Parse());
-            var root = parser.GetRootNode();
-            root.ToFunction(out var function);
+            var function = value.ToFunction();
             var functionD = function.GetDerivative("x");
             functionD = functionD.Reduce();
             Console.WriteLine("the x partial  derivative of " + value + " is \n" + functionD.ToString());
