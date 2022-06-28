ad03b91238a54f2f4ac50b28be753638aaa398c3
xiazeye <969919860@qq.com>
2022-06-28 23:42:07 +0800

    change Param from class to struct

--- MathParser/MathParser/Functions/Param.cs
+++ MathParser/MathParser/Functions/Param.cs
@@ -1,14 +1,29 @@
-﻿namespace TokenParser.Functions
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TokenParser.Functions
 {
-    public class Param
+    public struct Param:IEqualityComparer<Param>
     {
         public readonly string Name;
-        public readonly float Value = 0;
-
+        public readonly float Value;
+        public static readonly Param None=new Param("None",0);
         public Param(string name, float value)
         {
             Name = name;
             Value = value;
+        }
+
+
+        public bool Equals(Param x, Param y)
+        {
+            return x.Name == y.Name && x.Value.Equals(y.Value);
+        }
+
+        public int GetHashCode(Param obj)
+        {
+            return HashCode.Combine(obj.Name, obj.Value);
         }
     }
 }
