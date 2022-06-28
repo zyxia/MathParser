6dc5a177d7ffadbde4f866cfebf6cbda78646225
xiazeye <969919860@qq.com>
2022-06-28 23:28:50 +0800

    feat: add banchmark

---
+++ MathParser/BenchMark/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BenchmarkDotNet.Running;
+
+namespace BenchMark
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
+ 
