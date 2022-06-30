082789f3b77b1208bc498ffb63eb97caa8fffd41
xiazeye <969919860@qq.com>
2022-07-01 01:03:19 +0800

    test ci

--- MathParser/BenchMark/Benchs.cs
+++ MathParser/BenchMark/Benchs.cs
@@ -1,15 +1,29 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Linq;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Validators;
 using TokenParser;
 using TokenParser.Functions;
+
+public class AllowNonOptimized : ManualConfig
+{
+    public AllowNonOptimized()
+    {
+        Add(JitOptimizationsValidator.DontFailOnError); // ALLOW NON-OPTIMIZED DLLS
+        Add(DefaultConfig.Instance.GetLoggers().ToArray()); // manual config has no loggers by default
+        Add(DefaultConfig.Instance.GetExporters().ToArray()); // manual config has no exporters by default
+        Add(DefaultConfig.Instance.GetColumnProviders().ToArray()); // manual config has no columns by default
+    }
+}
+
 
 namespace BenchMark
 {
     [MemoryDiagnoser]
+    [Config(typeof(AllowNonOptimized))]
     public class Benchs
-    { 
+    {
         [Benchmark]
-        public Function ToFunction() =>  "(sin(x)-cos(y-x))".ToFunction();
-    
-        
+        public Function ToFunction() => "(sin(x)-cos(y-x))".ToFunction();
     }
 }
