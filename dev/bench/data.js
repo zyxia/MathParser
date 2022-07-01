f9cffe76cb18e2dea20b2e2ce31d6749cf54e062
github-action-benchmark <github@users.noreply.github.com>
2022-07-01 22:19:59 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 82c07d6544ea22d5c3322d3a76f6ffd72f8c7941

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1656713006531,
+  "lastUpdate": 1656713999008,
   "repoUrl": "https://github.com/zyxia/MathParser",
   "entries": {
     "Benchmark.Net Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 3748.2316873611944,
             "unit": "ns",
             "range": "± 106.2893168424241"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "969919860@qq.com",
+            "name": "xiazeye",
+            "username": "zyxia"
+          },
+          "committer": {
+            "email": "969919860@qq.com",
+            "name": "xiazeye",
+            "username": "zyxia"
+          },
+          "distinct": true,
+          "id": "82c07d6544ea22d5c3322d3a76f6ffd72f8c7941",
+          "message": "remove unused file",
+          "timestamp": "2022-07-02T06:16:54+08:00",
+          "tree_id": "146d84d336c90d39ecbd053f55184d9ca6aaa706",
+          "url": "https://github.com/zyxia/MathParser/commit/82c07d6544ea22d5c3322d3a76f6ffd72f8c7941"
+        },
+        "date": 1656713998049,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BenchMark.Benchs.ToFunction",
+            "value": 3311.7597479949127,
+            "unit": "ns",
+            "range": "± 109.44243795247858"
           }
         ]
       }
