8cd0bea3b542b96c75c4da61790fdef1e67f2b8c
github-action-benchmark <github@users.noreply.github.com>
2022-07-01 22:03:26 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 68efdcc2f4abc04ca531a09ea86da9d3d0b195a6

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1656713006531,
+  "repoUrl": "https://github.com/zyxia/MathParser",
+  "entries": {
+    "Benchmark.Net Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "969919860@qq.com",
+            "name": "xiazeye",
+            "username": "zyxia"
+          },
+          "committer": {
+            "email": "969919860@qq.com",
+            "name": "xiazeye",
+            "username": "zyxia"
+          },
+          "distinct": true,
+          "id": "68efdcc2f4abc04ca531a09ea86da9d3d0b195a6",
+          "message": "test",
+          "timestamp": "2022-07-02T01:06:31+08:00",
+          "tree_id": "6665cd26c356a88779c7d9dfe72df72b4234cc20",
+          "url": "https://github.com/zyxia/MathParser/commit/68efdcc2f4abc04ca531a09ea86da9d3d0b195a6"
+        },
+        "date": 1656713006114,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BenchMark.Benchs.ToFunction",
+            "value": 3748.2316873611944,
+            "unit": "ns",
+            "range": "± 106.2893168424241"
+          }
+        ]
+      }
+    ]
+  }
+}
